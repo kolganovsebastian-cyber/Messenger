c8c87b53b037863c36a93618de4fa1083cc4f2ca
Bannen Essen <kolganovsebastian@gmail.com>
2025-09-12 19:53:39 +0200

    Changed Ports

--- serversocket.py
+++ serversocket.py
@@ -67,7 +67,7 @@ def check_client(socket_client):
 
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(("0.0.0.0", 8100))
+socket.bind(("0.0.0.0", 8200))
 socket.listen()
 
 
